7bf463344dffc0498d7ea0a37eb4f1b39b594a85
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2024-01-27 20:34:32 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,11 @@ fn exec(task: String) {
         Ok(output) => {
             let stdout = String::from_utf8(output.stdout).unwrap();
             let _ = MUTEX.lock();
-            println!("-> {} {}", start_time.format("%Y-%m-%d %H:%M:%S%.3f%z"), task);
+            println!(
+                "-> {} {}",
+                start_time.format("%Y-%m-%d %H:%M:%S%.3f%z"),
+                task
+            );
             println!("{}", stdout);
             // output.status
         }

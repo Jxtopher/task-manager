1b475fc458ae8cc3d9a395c89901fe7a1d452069
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2024-03-30 00:18:20 +0100

    Directory backlog support

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
 # Queue run
 
-**Keywords**: backlog, executing tasks
+Manager task execution queues in parallel with a quenue depending on CPU/RAM load.
 
-Management of heavy task processing in parallel with a quenue depending on CPU/RAM load.
 
 ## Usage:
 
+* Execute all tasks listed in `protein-calculation.bl` with up to three processes in parallel (`-j`). The backlog can be updated on the fly if you add the tasks at the end.
+
 ```shell
-./qrun -f protein-calculation.bl
+qrun -b protein-calculation.bl -j 3
+```
+
+* `qrun` accepts a directory with a multiple of backlogs.
+
+```shell
+qrun -b /tmp/backlog
+```
+
+* `qrun` can work in demon mode (`-d`). When tasks are added to the backlog, the tasks will be detected and executed.
+
+```shell
+qrun -b /tmp/backlog -j 5 -d
 ```
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use chrono::Utc;
 use clap::Parser;
 use env_logger;
 use log::error;
-use std::fs::metadata;
+use std::fs;
 use std::path::Path;
 use std::process::Command;
 use std::sync::Mutex;
@@ -55,6 +55,17 @@ fn exec(task: String) {
     }
 }
 
+fn thread_visitor(thread_pool: &mut Vec<thread::JoinHandle<()>>) {
+    let mut index = 0;
+    while index < thread_pool.len() {
+        if thread_pool[index].is_finished() {
+            thread_pool.remove(index);
+        } else {
+            index += 1;
+        }
+    }
+}
+
 fn main() -> Result<(), std::io::Error> {
     env_logger::init();
     let args: Args = Args::parse();
@@ -63,48 +74,88 @@ fn main() -> Result<(), std::io::Error> {
     let mut is_locked: bool;
     let mut thread_pool: Vec<thread::JoinHandle<()>> = Vec::new();
 
+    let backlog = fs::metadata(&args.backlog);
+    let backlog_path: String = args.backlog.clone();
+    let mut backlog_file: String = args.backlog.clone();
+    match backlog {
+        Ok(_) => (),
+        Err(_) => {
+            error!("File or directory path \"{}\" does not exist", args.backlog);
+            return Ok(());
+        }
+    }
+
+    let is_dir = backlog.unwrap().is_dir();
+    if is_dir {
+        backlog_file.clear();
+    }
+
     while args.demon || has_tasks || thread_pool.len() > 0 {
-        match metadata(&args.backlog) {
-            Ok(_) => (),
-            Err(_) => {
-                error!("File path \"{}\" does not exist", args.backlog);
-                return Ok(());
+        if is_dir && backlog_file.is_empty() {
+            let paths = fs::read_dir(&backlog_path);
+            let extension: String = String::from("bl");
+            for entry in paths.unwrap() {
+                let path = entry?.path();
+                if path.extension() != None
+                    && path.extension().unwrap().to_string_lossy() == extension
+                {
+                    let start_time = Utc::now();
+                    let mutex = MUTEX.lock();
+                    println!(
+                        "-> {} backlog: {}",
+                        start_time.format("%Y-%m-%d %H:%M:%S%.3f%z"),
+                        path.display()
+                    );
+                    drop(mutex);
+                    backlog_file = path.display().to_string();
+                    break;
+                }
             }
         }
 
+        if backlog_file.is_empty() {
+            if !args.demon {
+                has_tasks = false;
+            }
+            thread_visitor(&mut thread_pool);
+            thread::sleep(Duration::from_secs(1));
+            continue;
+        }
+
+        // Read the backlog
+        let mut tasks = rw_file::read(&backlog_file);
+
         // Verify that the backlog file is not being edited
-        let path = Path::new(&args.backlog);
+        let path = Path::new(&backlog_file);
         let parent = path.parent().unwrap().to_str().unwrap();
         let swapfile_filename = path.file_name().unwrap().to_str().unwrap();
 
-        let swapfile = format!("{parent}/.{swapfile_filename}.swp");
-        match metadata(&swapfile) {
+        match fs::metadata(format!("{parent}/.{swapfile_filename}.swp")) {
             Ok(_) => is_locked = true,
             Err(_) => is_locked = false,
         }
 
-        // Read the backlog
-        let mut tasks = rw_file::read(&args.backlog);
-
         if tasks.len() == 0 {
             // No more tasks need to be executed in the backlog
-            has_tasks = false;
+            if is_dir {
+                match fs::remove_file(&backlog_file) {
+                    Ok(_) => (),
+                    Err(_) => (),
+                }
+                backlog_file.clear();
+            } else {
+                has_tasks = false;
+            }
         } else if !is_locked && thread_pool.len() < args.jobs {
-            let task = tasks[0].to_string();
-            tasks.remove(0);
-            rw_file::write(&args.backlog, &tasks);
-            thread_pool.push(thread::spawn(|| exec(task)));
+            while thread_pool.len() < args.jobs {
+                let task = tasks[0].to_string();
+                tasks.remove(0);
+                thread_pool.push(thread::spawn(|| exec(task)));
+            }
+            rw_file::write(&backlog_file, &tasks);
         }
 
-        // Thread visitor
-        let mut index = 0;
-        while index < thread_pool.len() {
-            if thread_pool[index].is_finished() {
-                thread_pool.remove(index);
-            } else {
-                index += 1;
-            }
-        }
+        thread_visitor(&mut thread_pool);
 
         thread::sleep(Duration::from_secs(1));
     }
